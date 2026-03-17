79eb2b20d1f7a47885d9276b9e4bd286c3f70757
josecarlostejedor <josecarlostejedor@gmail.com>
2026-03-17 17:52:44 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { createServer as createViteServer } from "vite";
 import path from "path";
 import multer from "multer";
 import fs from "fs";
-import { extractTextFromPDF, extractTextFromDocx } from "./src/lib/parser.js";
+import { extractTextFromPDF, extractTextFromDocx } from "./src/lib/parser";
 
 // Increase limit to 4MB to be safe with Vercel's 4.5MB payload limit
 const upload = multer({ 
